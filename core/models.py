7bfa4d9fe005ef6d9c4b8b1a847cfbd372ec1379
ShivamMalpani <shivammalpani@iitbhilai.ac.in>
2022-08-20 11:39:01 +0530

    new folders

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_id = models.IntegerField()
+    profile_image = models.ImageField(upload_to='profile_images',default='blank_profile.png')
+    Bio = models.TextField()
+    location = models.CharField(max_length=20,blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(requests):
     return render(requests,'index.html')
+
+def signup(requests):
+    return render(requests,'signup.html')
